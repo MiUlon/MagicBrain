952c480c233dca697796446a779fb4d84ca1ff13
Mindaugas Ulonas <mulonas@Mindaugass-MacBook-Air.local>
2023-01-11 15:18:13 +0200

    adding face box

--- magicbrain/src/Components/ImageLinkForm/ImageLinkForm.js
+++ magicbrain/src/Components/ImageLinkForm/ImageLinkForm.js
@@ -4,7 +4,7 @@ import './ImageLinkForm.css';
 const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
     return (
         <div>
-            <p className='f3'>{'This Magic Brain app will detect faces in your picture. Give it a try!'}</p>
+            <p className='f3'>{'This Magic Brain app will detect face in your pictures. Give it a try!'}</p>
             <div className='center'>
                 <div className='center form pa4 br3 shadow-5'>
                     <input className='f4 pa2 w-70 center' type='text' onChange={onInputChange}></input>
