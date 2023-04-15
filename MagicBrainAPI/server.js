5141e43b7229f9e23fa14e77d57ceb6f2843e6de
Mindaugas Ulonas <mindegi15@gmail.com>
2023-04-15 07:26:19 +0300

    adding transaction to Register API

--- MagicBrainAPI/server.js
+++ MagicBrainAPI/server.js
@@ -68,16 +68,29 @@ app.post('/signin', (req, res) => {
 
 app.post('/register', (req, res) => {
     const { name, email, password } = req.body;
-    postgreSQL('users')
-        .returning('*')
-        .insert({
-            name: name,
-            email: email,
-            joined: new Date()
+    const hash = bcrypt.hashSync(password);
+    postgreSQL.transaction(trx => {
+        trx.insert({
+            hash: hash,
+            email: email
         })
-        .then(user => {
-            res.json(user[0]);
+        .into('login')
+        .returning('email')
+        .then(loginEmail => {
+            return trx('users')
+                .returning('*')
+                .insert({
+                    name: name,
+                    email: loginEmail[0].email,
+                    joined: new Date()
+                })
+                .then(user => {
+                    res.json(user[0]);
+            })
         })
+        .then(trx.commit)
+        .catch(trx.rollback)
+    })
         .catch(err => res.status(400).json('Cannot register.'));
 });
 
